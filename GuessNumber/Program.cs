24a3abf0de5bffe95328360e7a97c8098ce40377
jmosqueiro <furosktim@icloud.com>
2019-10-28 12:17:41 +0100

    HW

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
