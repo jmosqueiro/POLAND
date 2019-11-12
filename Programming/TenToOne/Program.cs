4467c5ab533acec95d35bc29fe46ae3977f93298
jmosqueiro <furosktim@icloud.com>
2019-11-12 10:21:22 +0100

    Add Power code

--- Programming/TenToOne/Program.cs
+++ Programming/TenToOne/Program.cs
@@ -13,11 +13,30 @@ namespace TenToOne
                 counter--;
             } */
 
-            int counter = 0;
+            /* int counter = 0;
             while (counter < 10)
             {
                 Console.WriteLine("{0} Hello World!",counter);
                 counter=counter+2;
+            } */
+
+            int a;
+
+            Console.Write("Number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            int t= 0;
+            for (int i = 0; i < 11; i++)
+            {
+                if (i == 0)
+                {
+                   t = 1; 
+                }
+                else
+                {
+                    t *= a;
+                }
+                
+                Console.WriteLine("{0} ^ {1} = {2}",a, i, t);
             }
                    
         }
