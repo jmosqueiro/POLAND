25127e24bd42fa4245e5f7749ba6a0033bee175b
jmosqueiro <furosktim@icloud.com>
2019-11-12 08:48:43 +0100

    Create TenToOne Prj

---
+++ Programming/TenToOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TenToOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
