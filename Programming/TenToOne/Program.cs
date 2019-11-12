b7f7d005e8278f6caeedf12e4fb696ef19402aba
jmosqueiro <furosktim@icloud.com>
2019-11-12 09:37:00 +0100

    Add Cycle 2

--- Programming/TenToOne/Program.cs
+++ Programming/TenToOne/Program.cs
@@ -6,12 +6,21 @@ namespace TenToOne
     {
         static void Main(string[] args)
         {
-            int counter = 10;
+            /* int counter = 10;
             while (counter > 0)
             {
                 Console.WriteLine("{0} Hello World!",counter);
                 counter--;
+            } */
+
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine("{0} Hello World!",counter);
+                counter=counter+2;
             }
+                   
         }
     }
+   
 }
