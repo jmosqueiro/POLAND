5f44c63a6efe76c90db6cc65f8338ed996a97cf9
jmosqueiro <furosktim@icloud.com>
2019-11-19 08:37:11 +0100

    Add Steps to new C# project

---
+++ Programming/Steps to new project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Steps_to_new_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. mkdir the project name");
+            Console.WriteLine("2. cd -> new project folder");
+            Console.WriteLine("3. dotnet new console");
+            Console.WriteLine("4. done");
+        }
+    }
+}
