94da39b9bf6ed740f09c8c075b7f7f5370bef67c
jmosqueiro <furosktim@icloud.com>
2019-11-26 12:11:07 +0100

    add Matrix exercise

---
+++ Programming/Arrays_matrix_auto1and0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Arrays_matrix_auto1and0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tabA = new int[10,10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    //if (i % 2 == 0)
+                    if(i == j)
+                    {
+                        tabA[i,j] = 1;
+                    }                 
+                    else
+                    {
+                        tabA[i,j] = 0;
+                    }   
+                    System.Console.Write("{0}", tabA[i,j]);
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
